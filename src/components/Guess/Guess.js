66d332052e6fa49a258d2a6144637d43faec8217
Michael Foss <michael@mikefoss.com>
2024-10-03 12:43:02 -0400

    Render stylized guesses with separate letters

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Guess({ value }) {
-  return <p className="guess">{value}</p>;
+  const letters = value.split('').map((letter) => ({
+    id: crypto.randomUUID(),
+    letter,
+  }));
+  return (
+    <p className="guess">
+      {letters.map(({ id, letter }) => (
+        <span key={id} className="cell">
+          {letter}
+        </span>
+      ))}
+    </p>
+  );
 }
 
 export default Guess;
