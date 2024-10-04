8469c1f8dd81f247deac4b2d279259d699adad70
Michael Foss <michael@mikefoss.com>
2024-10-04 07:56:53 -0400

    Track stats for nerds

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
 import Game from '../Game';
 import Header from '../Header';
 
 function App() {
+  const [gamesPlayed, setGamesPlayed] = useState(0);
+  const [gamesWon, setGamesWon] = useState(0);
+  const [guessesMade, setGuessesMade] = useState(0);
+  const [guessesMadeForWins, setGuessesMadeForWins] = useState(0);
+
+  const handleGameOver = (isWinner, totalGuesses = 0) => {
+    setGamesPlayed(gamesPlayed + 1);
+    if (isWinner) {
+      setGamesWon(gamesWon + 1);
+      setGuessesMadeForWins(guessesMadeForWins + totalGuesses);
+    }
+  };
+
+  const handleGuess = () => {
+    setGuessesMade(guessesMade + 1);
+  };
+
   return (
     <div className="wrapper">
       <Header />
